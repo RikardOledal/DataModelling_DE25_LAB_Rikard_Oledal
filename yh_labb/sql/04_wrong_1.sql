29a5f47432ab553c79d8d976740789eb9788a71b
RIKARD OLEDAL <rikardoledal@gmail.com>
2026-01-27 14:45:24 +0100

    Update querrys

---
+++ yh_labb/sql/04_wrong_1.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+    "Class" (
+        class_id,
+        educatorlead_id,
+        program_id,
+        ind_course_id,
+        class_start
+    )
+VALUES
+    (1, 1, 1, NULL, '2024-08-01');
